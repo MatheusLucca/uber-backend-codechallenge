de981a564d8fd4432da6bed1cd6de5222311677a
Matheus Lucca <mat.lualves@gmail.com>
2023-11-09 20:02:51 -0300

    fix Unit Tests

--- src/test/java/com/matlucca/emailservice/application/EmailSenderServiceTest.java
+++ src/test/java/com/matlucca/emailservice/application/EmailSenderServiceTest.java
@@ -2,6 +2,7 @@ package com.matlucca.emailservice.application;
 
 import com.matlucca.emailservice.adapters.EmailSenderGateway;
 import com.matlucca.emailservice.core.exceptions.EmailServiceException;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,6 +10,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyString;
@@ -18,16 +20,12 @@ class EmailSenderServiceTest {
 
     public static final String MAIL = "mat.lualves@gmail.com";
     public static final String TESTE = "Teste";
+
     @InjectMocks
     private EmailSenderService emailSenderService;
 
     @Mock
     private EmailSenderGateway mainEmailSenderGateway;
-
-    @Mock
-    private EmailSenderGateway optEmailSenderGateway;
-
-
 
 
     @BeforeEach
@@ -38,19 +36,15 @@ class EmailSenderServiceTest {
     @Test
     @DisplayName("When send email then email sent sucessfully")
     void whenSendEmailThenEmailSentSucessfully() {
-        doNothing().when(mainEmailSenderGateway).sendEmail(anyString(), anyString(), anyString());
-
-
         emailSenderService.sendEmail(MAIL, TESTE, TESTE);
         Mockito.verify(mainEmailSenderGateway, times(1))
-                .sendEmail(anyString(), anyString(), anyString());
+                .sendEmail(any(), any(), any());
     }
 
     @Test
     @DisplayName("When send email then main fail and opt is used")
     void whenSendEmailThenMainFailAndOptIsUsed(){
-        doThrow(new EmailServiceException("ERRO")).when(mainEmailSenderGateway).sendEmail(anyString(), anyString(), anyString());
-        doNothing().when(optEmailSenderGateway).sendEmail(anyString(), anyString(), anyString());
+        doThrow(new EmailServiceException("ERRO")).when(mainEmailSenderGateway).sendEmail(any(), any(), any());
         try{
             emailSenderService.sendEmail(MAIL, TESTE, TESTE);
         } catch (EmailServiceException ex){
@@ -65,7 +59,6 @@ class EmailSenderServiceTest {
     @DisplayName("When send email then main and opt fail")
     void whenSendEmailThenReturnEmailServiceException(){
         doThrow(new EmailServiceException("ERRO")).when(mainEmailSenderGateway).sendEmail(anyString(), anyString(), anyString());
-        doThrow(new EmailServiceException("ERRO")).when(optEmailSenderGateway).sendEmail(anyString(), anyString(), anyString());
 
         assertThrows(EmailServiceException.class, () -> {
             emailSenderService.sendEmail(MAIL, TESTE, TESTE);
